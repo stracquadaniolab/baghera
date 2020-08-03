d661b570b3aa2a34373a674775bd56f0d9bf2c6d
violafanfani <viola.fanfani@gmail.com>
2020-08-03 10:04:51 +0100

    Bump version: 2.1.4 → 2.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ copyright = 'Viola Fanfani'
 author = 'Viola Fanfani'
 
 # The short X.Y version
-version = '2.1.4'
+version = '2.1.5'
 # The full version, including alpha/beta/rc tags
-release = '2.1.4'
+release = '2.1.5'
 
 
 # -- General configuration ---------------------------------------------------
